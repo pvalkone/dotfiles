512407b96827b2b41ce0989837729a4e1a6b2517
Petteri Valkonen <petteri.valkonen@iki.fi>
2016-02-01 19:57:23 +0200

    Make lock screen more blurred

--- bin/bin/lock.sh
+++ bin/bin/lock.sh
@@ -23,8 +23,9 @@ SCREENSHOT="scrot $IMAGE" # 0.46s
 #BLURTYPE="0x5" # 7.52s
 #BLURTYPE="0x2" # 4.39s
 #BLURTYPE="5x2" # 3.80s
-BLURTYPE="2x8" # 2.90s
+#BLURTYPE="2x8" # 2.90s
 #BLURTYPE="2x3" # 2.92s
+BLURTYPE="0x8"
 
 # Get the screenshot, add the blur and lock the screen with it
 $SCREENSHOT
