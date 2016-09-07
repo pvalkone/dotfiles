48f6db7b65c504ae3b716562c2b203e64c2b2cd4
Petteri Valkonen <petteri.valkonen@iki.fi>
2016-09-07 12:25:42 +0300

    Set repl-options

--- leiningen/.lein/profiles.clj
+++ leiningen/.lein/profiles.clj
@@ -1,1 +1,7 @@
-{:user {:plugins [[lein-ancient "0.6.8"]]}}
+{:user {:plugins [[org.clojure/tools.trace "0.7.9"]
+                  [clj-stacktrace "0.2.8"]
+                  [lein-ancient "0.6.8"]]}
+        :repl-options {:init (do
+                               (alter-var-root #'*out* (constantly *out*))
+                               (require '[clojure.tools.trace :refer [trace]]))
+                       :caught clj-stacktrace.repl/pst+}}
