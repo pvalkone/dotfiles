bbb7ebafb889aa3ef6b0fd63b25f7bd037eb4265
Petteri Valkonen <petteri.valkonen@iki.fi>
2016-09-13 15:33:20 +0300

    Use lein-auto plugin

--- leiningen/.lein/profiles.clj
+++ leiningen/.lein/profiles.clj
@@ -1,6 +1,7 @@
 {:user {:plugins [[org.clojure/tools.trace "0.7.9"]
                   [clj-stacktrace "0.2.8"]
-                  [lein-ancient "0.6.8"]]}
+                  [lein-ancient "0.6.8"]
+                  [lein-auto "0.1.2"]]}
         :repl-options {:init (do
                                (alter-var-root #'*out* (constantly *out*))
                                (require '[clojure.tools.trace :refer [trace]]))
