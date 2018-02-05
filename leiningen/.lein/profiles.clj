ee5bdc0f677893bf8a4309e32b3d1b59e41e1e05
Petteri Valkonen <petteri.valkonen@iki.fi>
2018-02-05 10:36:58 +0200

    Fix indentation

--- leiningen/.lein/profiles.clj
+++ leiningen/.lein/profiles.clj
@@ -3,14 +3,15 @@
                   [lein-autoreload "0.1.1"]
                   [lein-kibit "0.1.6-beta2"]
                   [venantius/ultra "0.5.0"]]}
-        :dependencies [[org.clojure/tools.trace "0.7.9"]
-                       [clj-stacktrace "0.2.8"]
-                       [slamhound "1.5.5"]]
-        :repl-options {:init (do
-                               (alter-var-root #'*out* (constantly *out*))
-                               (require '[clojure.tools.trace :refer [trace]]))
-                       :caught clj-stacktrace.repl/pst+}
+ :dependencies [[org.clojure/tools.trace "0.7.9"]
+                [clj-stacktrace "0.2.8"]
+                [slamhound "1.5.5"]]
+ :repl-options {:init (do
+                        (alter-var-root #'*out* (constantly *out*))
+                        (require '[clojure.tools.trace :refer [trace]]))
+                :caught clj-stacktrace.repl/pst+}
  :repl {:repl-options {:prompt (fn [ns]
                                  (str "\033[1;32m"
                                       ns "=>"
                                       "\033[0m "))}}}
+
