0c2f140e9f07083fd992e5352027321735d1cfba
Petteri Valkonen <petteri.valkonen@iki.fi>
2018-09-05 09:17:53 +0300

    Remove :repl key and fix indentation

--- leiningen/.lein/profiles.clj
+++ leiningen/.lein/profiles.clj
@@ -3,15 +3,15 @@
                   [lein-autoreload "0.1.1"]
                   [lein-kibit "0.1.6"]
                   [venantius/ultra "0.5.2"]]}
- :dependencies [[org.clojure/tools.trace "0.7.9"]
-                [clj-stacktrace "0.2.8"]
-                [slamhound "1.5.5"]]
- :repl-options {:init (do
-                        (alter-var-root #'*out* (constantly *out*))
-                        (require '[clojure.tools.trace :refer [trace]]))
-                :caught clj-stacktrace.repl/pst+}
- :repl {:repl-options {:prompt (fn [ns]
+        :dependencies [[org.clojure/tools.trace "0.7.9"]
+                       [clj-stacktrace "0.2.8"]
+                       [slamhound "1.5.5"]]
+        :repl-options {:init (do
+                               (alter-var-root #'*out* (constantly *out*))
+                               (require '[clojure.tools.trace :refer [trace]]))
+                       :caught clj-stacktrace.repl/pst+
+                       :prompt (fn [ns]
                                  (str "\033[1;32m"
                                       ns "=>"
-                                      "\033[0m "))}}}
+                                      "\033[0m "))}}
 
