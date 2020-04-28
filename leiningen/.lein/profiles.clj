f148701b9b4653dc25ab6c522eefb96d3bbddcf7
Petteri Valkonen <petteri.valkonen@iki.fi>
2020-04-28 08:34:56 +0300

    Update Leiningen dependencies

--- leiningen/.lein/profiles.clj
+++ leiningen/.lein/profiles.clj
@@ -1,10 +1,10 @@
 {:user {:plugins [[lein-ancient "0.6.15"]
                   [lein-auto "0.1.3"]
                   [lein-autoreload "0.1.1"]
-                  [lein-kibit "0.1.6"]
-                  [lein-nvd "0.5.5"]
-                  [venantius/ultra "0.5.2"]]}
-        :dependencies [[org.clojure/tools.trace "0.7.9"]
+                  [lein-kibit "0.1.8"]
+                  [lein-nvd "1.4.0"]
+                  [venantius/ultra "0.6.0"]]}
+        :dependencies [[org.clojure/tools.trace "0.7.10"]
                        [clj-stacktrace "0.2.8"]
                        [slamhound "1.5.5"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}
