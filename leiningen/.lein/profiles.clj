223c3d7ed0e2f4a90b80b44f66c28ea0b74a2c58
Petteri Valkonen <petteri.valkonen@iki.fi>
2020-09-18 09:59:38 +0300

    Update lein-nvd to version 1.4.1

--- leiningen/.lein/profiles.clj
+++ leiningen/.lein/profiles.clj
@@ -2,7 +2,7 @@
                   [lein-auto "0.1.3"]
                   [lein-autoreload "0.1.1"]
                   [lein-kibit "0.1.8"]
-                  [lein-nvd "1.4.0"]
+                  [lein-nvd "1.4.1"]
                   [venantius/ultra "0.6.0"]]}
         :dependencies [[org.clojure/tools.trace "0.7.10"]
                        [clj-stacktrace "0.2.8"]
