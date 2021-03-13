beda696c9c4e2ddb5042b274bc3ef08987fd7c18
Petteri Valkonen <petteri.valkonen@iki.fi>
2021-03-13 11:00:19 +0200

    Add Tarsnap backup script

---
+++ bin/bin/tarsnap-backup.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+backup() {
+  source=$1
+  shift
+  rest="$*"
+  if [ -z "$rest" ]; then
+    rest=".git"
+  fi
+  excludes=$(for e in $rest; do /bin/echo " --exclude '$e'" | tr -d '\n'; done)
+  prefix=$(echo "$source" | sed -e 's|[/\.]|_|g')
+
+  echo "Backing up $source"
+
+  eval "/opt/bin/snapbak2 \
+    --cachedir /var/cache/tarsnap \
+    --type daily \
+    --prefix $prefix \
+    $excludes \
+    --keep 14 \
+    $source"
+}
+
+backup "/boot/loader.conf"
+backup "/root" ".cache" "dead.letter"
+backup "/etc"
+backup "/usr/home" "*.git" ".cache" "dead.letter" "pvalkone/dotfiles"
+backup "/usr/local/etc"
+backup "/usr/local/git" "dummy-value-to-keep-snapbak2-happy"
+backup "/var/backups"
+backup "/var/cron/tabs"
+backup "/var/db/ports"
+backup "/var/db/sshguard"
+backup "/opt/etc"
