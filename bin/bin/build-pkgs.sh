69173963de1771beae9923b318c8810d34671516
Petteri Valkonen <petteri.valkonen@iki.fi>
2021-05-14 10:39:31 +0300

    Adapt script for building packages with Poudriere

--- bin/bin/build-pkgs.sh
+++ bin/bin/build-pkgs.sh
@@ -5,10 +5,11 @@ if [ -z "${CUSTOM_PORTS_DIR}" ]; then
   exit 1
 fi
 
-portshaker && \
-for port in ${BLACKLISTED_PORTS}; do echo "IGNORE=Blacklisted by ${0}" | tee "/usr/ports/${port}/Makefile.local"; done && \
-CUSTOM_PORTS=$(find "${CUSTOM_PORTS_DIR}" -type d -not -path "*/\.*" -not -path '*/py-*' -mindepth 2 -maxdepth 2 | sed "s|\\${CUSTOM_PORTS_DIR}/||")
-PORTS_TO_BUILD=$(echo "${ADDITIONAL_PORTS} ${CUSTOM_PORTS}" | grep -vE "($(echo "${IGNORED_PORTS}" | tr -d '\n' | tr ' ' '|'))" | ghead -c -1 | tr '\n' ' ')
-echo "Building ${PORTS_TO_BUILD} using Synth"
-eval "synth just-build ${PORTS_TO_BUILD}" && \
-synth rebuild-repository
+BUILD_LIST_FILE="/usr/local/etc/poudriere.d/12-amd64.list"
+
+/usr/local/bin/portshaker -q && \
+for port in ${BLACKLISTED_PORTS}; do echo "IGNORE=Blacklisted by ${0}" | tee "/usr/local/poudriere/ports/main/${port}/Makefile.local"; done && \
+CUSTOM_PORTS=$(find "${CUSTOM_PORTS_DIR}" -type d -not -path "*/\.*" -not -path '*/py-*' -mindepth 2 -maxdepth 2 | sed "s|\\${CUSTOM_PORTS_DIR}/||") && \
+PORTS_TO_BUILD=$(echo "${ADDITIONAL_PORTS} ${CUSTOM_PORTS}" | grep -vE "($(echo "${IGNORED_PORTS}" | tr -d '\n' | tr ' ' '|'))") && \
+echo "${PORTS_TO_BUILD}" > "${BUILD_LIST_FILE}" && \
+/usr/local/bin/poudriere -N bulk -j 12-amd64 -p main -f "${BUILD_LIST_FILE}"
