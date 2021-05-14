ef50c6fc631d9f9c9431a55b0e76f7e42eda52e7
Petteri Valkonen <petteri.valkonen@iki.fi>
2021-05-14 12:18:08 +0300

    Fix PORTS_TO_BUILD parsing

--- bin/bin/build-pkgs.sh
+++ bin/bin/build-pkgs.sh
@@ -10,6 +10,6 @@ BUILD_LIST_FILE="/usr/local/etc/poudriere.d/12-amd64.list"
 /usr/local/bin/portshaker -q && \
 for port in ${BLACKLISTED_PORTS}; do echo "IGNORE=Blacklisted by ${0}" | tee "/usr/local/poudriere/ports/main/${port}/Makefile.local"; done && \
 CUSTOM_PORTS=$(find "${CUSTOM_PORTS_DIR}" -type d -not -path "*/\.*" -not -path '*/py-*' -mindepth 2 -maxdepth 2 | sed "s|\\${CUSTOM_PORTS_DIR}/||") && \
-PORTS_TO_BUILD=$(echo "${ADDITIONAL_PORTS} ${CUSTOM_PORTS}" | grep -vE "($(echo "${IGNORED_PORTS}" | tr -d '\n' | tr ' ' '|'))") && \
+PORTS_TO_BUILD=$(printf "$(echo "${ADDITIONAL_PORTS}" | tr ' ' '\n')\n${CUSTOM_PORTS}" | grep -vE "($(echo "${IGNORED_PORTS}" | tr -d '\n' | tr ' ' '|'))" | sort) && \
 echo "${PORTS_TO_BUILD}" > "${BUILD_LIST_FILE}" && \
 /usr/local/bin/poudriere -N bulk -j 12-amd64 -p main -f "${BUILD_LIST_FILE}"
