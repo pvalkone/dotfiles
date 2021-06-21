f4816834238ab078e8b03e3fa6f35768668616d0
Petteri Valkonen <petteri.valkonen@iki.fi>
2021-06-21 08:58:02 +0300

    Restrict number of parallel build jobs
    Rust will occasionally fail to build because of resource starvation.
    
    See: https://forums.freebsd.org/threads/poudriere-never-completes-first-run-without-errors.78000/#post-486902

--- bin/bin/build-pkgs.sh
+++ bin/bin/build-pkgs.sh
@@ -12,4 +12,4 @@ for port in ${BLACKLISTED_PORTS}; do echo "IGNORE=Blacklisted by ${0}" | tee "/u
 CUSTOM_PORTS=$(find "${CUSTOM_PORTS_DIR}" -type d -not -path "*/\.*" -not -path '*/py-*' -mindepth 2 -maxdepth 2 | sed "s|\\${CUSTOM_PORTS_DIR}/||") && \
 PORTS_TO_BUILD=$(printf "$(echo "${ADDITIONAL_PORTS}" | tr ' ' '\n')\n${CUSTOM_PORTS}" | grep -vE "($(echo "${IGNORED_PORTS}" | tr -d '\n' | tr ' ' '|'))" | sort) && \
 echo "${PORTS_TO_BUILD}" > "${BUILD_LIST_FILE}" && \
-/usr/local/bin/poudriere -N bulk -j 12-amd64 -p main -f "${BUILD_LIST_FILE}"
+/usr/local/bin/poudriere -N bulk -j 12-amd64 -p main -f "${BUILD_LIST_FILE}" -J 1:2 -t
