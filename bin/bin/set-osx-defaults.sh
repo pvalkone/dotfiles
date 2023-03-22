18c6ab96d1929f2c5828e99dcaa2c6edb0f1a946
Petteri Valkonen <petteri.valkonen@iki.fi>
2023-03-22 12:47:04 +0200

    Remove quotes

--- bin/bin/set-osx-defaults.sh
+++ bin/bin/set-osx-defaults.sh
@@ -13,7 +13,7 @@ defaults write com.apple.dock wvous-tr-corner -int 5 && \
 defaults write com.apple.dock wvous-tr-modifier -int 0 && \
 defaults write com.apple.dock wvous-br-corner -int 0 && \
 # Autohide the Dock when the mouse is out
-defaults write com.apple.dock autohide -bool "true" && \
+defaults write com.apple.dock autohide -bool true && \
 # Speed up autohide
 defaults write com.apple.dock autohide-time-modifier -float 0.15 && \
 killall Dock
