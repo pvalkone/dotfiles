23afbdb4ecceef8e29b07cbeb82058ab4f00e64a
Petteri Valkonen <petteri.valkonen@iki.fi>
2024-02-19 09:40:43 +0200

    Update set-osx-defaults.sh

--- osx/bin/set-osx-defaults.sh
+++ osx/bin/set-osx-defaults.sh
@@ -6,19 +6,28 @@ if [ "$(uname -s)" != "Darwin" ]; then
 fi
 
 # Hide filename extensions in Finder
-defaults write NSGlobalDomain AppleShowAllExtensions -bool false && \
+defaults write NSGlobalDomain AppleShowAllExtensions -bool false
 # Show hidden files
-defaults write com.apple.finder AppleShowAllFiles -bool true && \
+defaults write com.apple.finder AppleShowAllFiles -bool true
 # Don't default to saving documents to iCloud
-defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false && \
-killall Finder
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
 
 # Top right screen corner → Start screen saver
-defaults write com.apple.dock wvous-tr-corner -int 5 && \
-defaults write com.apple.dock wvous-tr-modifier -int 0 && \
-defaults write com.apple.dock wvous-br-corner -int 0 && \
+defaults write com.apple.dock wvous-tr-corner -int 5
+defaults write com.apple.dock wvous-tr-modifier -int 0
+defaults write com.apple.dock wvous-br-corner -int 0
 # Autohide the Dock when the mouse is out
-defaults write com.apple.dock autohide -bool true && \
+defaults write com.apple.dock autohide -bool true
 # Speed up autohide
-defaults write com.apple.dock autohide-time-modifier -float 0.15 && \
-killall Dock
+defaults write com.apple.dock autohide-time-modifier -float 0.15
+
+# Hide battery information in menu bar
+defaults write $HOME/Library/Preferences/com.apple.controlcenter.plist \
+  "NSStatusItem Visible Battery" -bool false
+
+# F1, F2, etc. behave as standard function keys
+defaults write NSGlobalDomain com.apple.keyboard.fnState -bool true
+
+for app in "Finder" "Dock"; do
+  killall "${app}"
+done
