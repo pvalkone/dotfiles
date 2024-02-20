99aca61fb9d186f1d93c589cb70d1bbfee8d452e
Petteri Valkonen <petteri.valkonen@iki.fi>
2024-02-20 19:38:46 +0200

    Build packages for FreeBSD 13

--- freebsd/bin/build-pkgs.sh
+++ freebsd/bin/build-pkgs.sh
@@ -5,11 +5,12 @@ if [ -z "${CUSTOM_PORTS_DIR}" ]; then
   exit 1
 fi
 
-BUILD_LIST_FILE="/usr/local/etc/poudriere.d/12-amd64.list"
+JAIL_NAME="13-amd64"
+BUILD_LIST_FILE="/usr/local/etc/poudriere.d/$JAIL_NAME.list"
 
 /usr/local/bin/portshaker -q && \
 for port in ${BLACKLISTED_PORTS}; do echo "IGNORE=Blacklisted by ${0}" | tee "/usr/local/poudriere/ports/main/${port}/Makefile.local"; done && \
 CUSTOM_PORTS=$(find "${CUSTOM_PORTS_DIR}" -type d -not -path "*/\.*" -not -path '*/py-*' -mindepth 2 -maxdepth 2 | sed "s|\\${CUSTOM_PORTS_DIR}/||") && \
 PORTS_TO_BUILD=$(printf "$(echo "${ADDITIONAL_PORTS}" | tr ' ' '\n')\n${CUSTOM_PORTS}" | grep -vE "($(echo "${IGNORED_PORTS}" | tr -d '\n' | tr ' ' '|'))" | sort) && \
 echo "${PORTS_TO_BUILD}" > "${BUILD_LIST_FILE}" && \
-/usr/local/bin/poudriere -N bulk -j 12-amd64 -p main -f "${BUILD_LIST_FILE}" -J 1:2 -t
+/usr/local/bin/poudriere -N bulk -j $JAIL_NAME -p main -f "${BUILD_LIST_FILE}" -J 1:2 -t
