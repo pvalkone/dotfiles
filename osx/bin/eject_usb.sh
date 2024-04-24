e8899022c2abd50bd4fd248527f4f24734e72ffa
Petteri Valkonen <petteri.valkonen@iki.fi>
2024-04-24 20:52:00 +0300

    Add script for ejecting a USB volume and cleaning up OS X cruft

---
+++ osx/bin/eject_usb.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+die() {
+  echo "$*" >&2
+  exit 1
+}
+
+is_user_root() {
+  [ "$(id -u)" -eq 0 ]
+}
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $(basename "$0") <USB volume>"
+  exit 255
+fi
+
+volume=$1
+readonly volume
+
+[ -d "$volume" ] || die "The volume doesn't seem to be mounted."
+
+device="$(df | grep "$volume" | awk '{print $1}')"
+readonly device
+
+[ -n "$device" ] || die "Couldn't determine device name."
+
+if ! is_user_root; then
+  sudo_cmd="$(command -v sudo)"
+  readonly sudo_cmd
+fi
+
+$sudo_cmd mdutil -i off "$volume" > /dev/null 2>&1
+$sudo_cmd dot_clean -m -n "$volume" 2> /dev/null
+
+diskutil quiet unmount "$device" || die "Failed to unmount device."
+
+drive="u:"
+readonly drive
+
+mtoolsrc=$HOME/.mtoolsrc
+readonly mtoolsrc
+
+echo "drive $drive file=\"$device\"" > "$mtoolsrc"
+$sudo_cmd mdeltree "$drive/.Spotlight-V100" "$drive/.fseventsd" "$drive/.Trashes" 2> /dev/null
+rm -f $mtoolsrc
+
+$sudo_cmd fatsort -q "$device" 2> /dev/null || die "Failed to sort disk."
+
+echo "Done. It's now safe to remove the USB drive."
