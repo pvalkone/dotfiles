8b8ae0b55a7f9931e3cb26869075feb1c4c6733d
Petteri Valkonen <petteri.valkonen@iki.fi>
2024-05-29 09:27:59 +0300

    Maximise window on startup

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,4 +1,10 @@
 local wezterm = require 'wezterm'
+local mux = wezterm.mux
+
+wezterm.on("gui-startup", function()
+  local _, _, window = mux.spawn_window{}
+  window:gui_window():maximize()
+end)
 
 local config = wezterm.config_builder()
 
